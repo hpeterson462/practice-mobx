3864a486c6499f6568f06be6ac449f2c25e620cc
Heather Peterson <hpeterson462@gmail.com>
2020-11-06 11:37:34 -0800

    stores provider

---
+++ src/stores/storesProvider.js
@@ -0,0 +1,19 @@
+import React, { createContext, useContext } from 'react';
+import { createBooksStore } from './booksStore';
+import { useLocalObservable } from 'mobx-react';
+
+const BooksContext = createContext(null);
+
+export const BookProvider = ({ children }) => {
+  const booksStore = useLocalObservable(() => createBooksStore());
+
+  return (
+    <BooksContext.Provider value={booksStore}>
+      {children}
+    </BooksContext.Provider>
+  );
+};
+
+export const useBooksStore = () => useContext(BooksContext);
+
+
